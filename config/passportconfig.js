fbfed365258a14e33e65ff2b92d3bdf80e75abd1
tanaygit0530 <tanaypatil0530@gmail.com>
2025-07-21 18:13:09 +0530

    Changes made in passportconfig

--- config/passportconfig.js
+++ config/passportconfig.js
@@ -6,7 +6,7 @@ const User = require('../models/user');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,         
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
+    callbackURL: process.env.CALLBACK_URL
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
